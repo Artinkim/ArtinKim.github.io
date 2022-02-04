e9638f2df0f67f9c372c890b4b5d5c9f113d04dd
Artinkim <37283700+Artinkim@users.noreply.github.com>
2022-02-03 16:50:40 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,7 +59,7 @@ function draw() { // this function code runs in infinite loop
         }
 
         // Apply specs and cigar
-        image(specs, singlePose.nose.x-800, singlePose.nose.y-140, 12, 125);
+        image(specs, singlePose.nose.x, singlePose.nose.y, 125, 125);
     }
     
     //background(200);
